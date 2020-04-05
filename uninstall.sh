ebed2dd22d22babff3b85a6f133be8bf3fbd99b3
Keith Wall <kwall@apache.org>
2020-04-05 16:02:41 +0100

    upd

--- uninstall.sh
+++ uninstall.sh
@@ -2,6 +2,7 @@
 
 oc delete crd -l app=enmasse,enmasse-component=iot
 oc delete crd -l app=enmasse --timeout=600s
+oc delete apiservices -l app=enmasse --timeout=600s
 oc delete clusterrolebindings -l app=enmasse
 oc delete clusterroles -l app=enmasse
 oc delete apiservices -l app=enmasse
